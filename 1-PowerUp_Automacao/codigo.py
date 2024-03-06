16dc6f7514b3a868400c533a990d606c216f076a
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-03-06 17:30:07 -0300

    a

--- 1-PowerUp_Automacao/codigo.py
+++ 1-PowerUp_Automacao/codigo.py
@@ -2,14 +2,3 @@
 # 1- entrar no sistema da empresa
   #https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
- import pyautogui
-  
-  # pyautogui.click -> clicar em algum lugar da tela
-  # pyautogui.write -> escrever um texto
-  # pyautogui.press -> pressionar 1 tecla do teclado
-  pyautogui.press("win")
-  pyautogui.write("chrome")
-  # Passo 02: Fazer login
-  # Passo 03: Importar a base de dados
-  # Passo 04: Cadastrar 1 produto
-  # Passo 05: Reoetr o processo de cadastro até acaabr a base de dados
